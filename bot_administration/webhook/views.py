d67d61d587b1292502776bb6f6c84a14a8f3cbd2
4rbshka <101452463+4rbshka@users.noreply.github.com>
2023-11-03 00:53:47 +0300

    Update views.py

--- bot_administration/webhook/views.py
+++ bot_administration/webhook/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .utils import parse_taiga_webhook
 
 
 class WebhookReceiver(APIView):
     def post(self, request, format=None):
         data = request.data  # Получаем данные из POST-запроса
         user_id = data['by']['id']
-        return [user_id, data]
+        return [user_id, parse_taiga_webhook(data)]
